a0badc05666bcf87d00e86bf45873e67d6889816
Luis <lecuevasp@gmail.com>
2024-07-01 17:56:05 -0400

    Agregue funcion multiplicar

--- suma.py
+++ suma.py
@@ -8,3 +8,13 @@ if __name__ == "__main__":
     b = input('ingrese otro numero ')
     resultado = fn_suma(int(a), int(b))  # Aquí puedes pasar los valores que desees
     print("La suma es:", resultado)
+
+# Crear funcion multiplicar
+def fn_multiplicar(a, b):
+    total_mult = a * b
+    return total_mult
+
+if __name__ == "__main__":
+    # Ejemplo de uso de la función
+    resultado = fn_multiplicar(int(a), int(b))  # Aquí puedes pasar los valores que desees
+    print("La multiplicacion es:", resultado)
